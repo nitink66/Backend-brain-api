5fc05bfe6bc8ca9ee4247a9d2c33eba159817d7e
nitink66 <60770391+nitink66@users.noreply.github.com>
2020-04-01 18:39:09 +0530

    added server files

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const cors = require('cors');
+
+const app = express();
+app.use(bodyparser.json());
+app.use(cors());
+
+const database = {
+    users:[
+        {
+            id:'123',
+            name:'john',
+            email:'john@gmail.com',
+            password:'cookies',
+            entries:0,
+            joined: new Date()
+        },
+        {
+            id:'124',
+            name:'sally',
+            email:'sally@gmail.com',
+            password:'bananas',
+            entries:0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/',(req,res)=>{
+    res.json(database.users);
+})
+
+
+app.post('/signin',(req,res)=>{
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json(database.users[0]);
+    }
+    else{
+        res.status(400).json('error logging in');
+    }
+   
+})
+
+
+app.post('/register',(req,res)=>{
+    const {name , email , password} = req.body;
+    database.users.push({
+            id:'125',
+            name:name,
+            email:email,
+            entries:0,
+            joined: new Date()
+
+    })
+    res.json(database.users[database.users.length-1])
+})
+
+
+app.post('/profile/:id',(req,res)=>{
+    const { id } =req.params;
+    let found = false;
+    database.users.forEach(user=>{
+        if(user.id === id){
+            found = true;
+          return res.json(user);
+        }
+    }) 
+    if(!found){
+        res.status(400).json('NOT FOUND'); 
+    }
+})
+
+app.put('/image',(req,res)=>{
+    const { id } =req.body;
+    let found = false;
+    database.users.forEach(user=>{
+        if(user.id === id){
+            found = true;
+            user.entries++
+          return res.json(user.entries);
+        }
+    }) 
+    if(!found){
+        res.status(400).json('NOT FOUND'); 
+    }
+})  
+
+
+app.listen(3000,()=>{
+    console.log("listening to port 3000");
+})
