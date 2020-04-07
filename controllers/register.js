ed1dc463f0ee238128305eac43a38a3a1cdcf25a
nitink66 <60770391+nitink66@users.noreply.github.com>
2020-04-07 09:27:53 +0530

    response

--- controllers/register.js
+++ controllers/register.js
@@ -26,7 +26,9 @@ const handleRegister= (req,res,db,bcrypt)=>{
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => console.log(err))
+    .catch(err =>{
+        res.status(400).json(err)
+    })
     
 }
 
